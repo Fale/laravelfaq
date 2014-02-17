ff6768faad2390395966503c14a9bfec9b26e491
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-02-17 12:13:06 +0100

    Add Faqs models and migration

--- app/database/migrations/2014_02_17_110647_create_faqs_table.php
+++ app/database/migrations/2014_02_17_110647_create_faqs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCategoriesTable extends Migration {
+class CreateFaqsTable extends Migration {
 
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCategoriesTable extends Migration {
      */
     public function up()
     {
-        Schema::create('categories', function(Blueprint $table) {
+        Schema::create('faqs', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('order');
             $table->string('name');
@@ -28,7 +28,7 @@ class CreateCategoriesTable extends Migration {
      */
     public function down()
     {
-        Schema::drop('categories');
+        Schema::drop('faqs');
     }
 
 }
---
+++ app/models/Faq.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+
+class Faq extends Eloquent {
+
+    public $timestamps = false;
+
+}
